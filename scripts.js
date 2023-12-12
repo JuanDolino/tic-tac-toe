9ddd08bc0c89839fdd8a8b9c59c1bdf179cae81a
Juan Vitor <juandolino8@gmail.com>
2023-12-11 22:02:02 -0300

    the code is now more clean and efficient

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-let playTime = "X", checkIfOWins, checkIfXWins, whoWins;
+let playTime = "X";
 
-let matrix =  [ ["", "", ""],
+let checkIfOWins, checkIfXWins, whoWins;
+
+let gameBoard =  [ ["", "", ""],
                 ["", "", ""],
                 ["", "", ""]]
 
@@ -14,33 +16,22 @@ function insertElement(element, position) {
   let row = element.parentElement.classList[1];
   image = document.createElement("img")
 
-  if(playTime == "X") {
-    image.setAttribute("src", "./assets/imageX.png")
-    image.setAttribute("class", "imageX")
 
-    matrix[row == "firstRow" ? 0 : row == "secondRow" ? 1 : row == "thirdRow" ? 2 : null][position] = "X"
-    
-    playTime = "O";
-  }
+  image.setAttribute("src", playTime == "X" ? "./assets/imageX.png" : "./assets/imageO.png");
+  image.setAttribute("class", playTime == "X" ? "imageX" : "imageO")
 
-  else {
-    image.setAttribute("src", "./assets/imageO.png")
-    image.setAttribute("class", "imageO")
-
-    matrix[row == "firstRow" ? 0 : row == "secondRow" ? 1 : row == "thirdRow" ? 2 : null][position] = "O"
-
-    playTime = "X";
-  }
+  gameBoard[row == "firstRow" ? 0 : row == "secondRow" ? 1 : row == "thirdRow" ? 2 : null][position] = playTime;
+  
+  playTime = playTime == "X" ? "O" : "X";
 
   element.appendChild(image)
   element.style.pointerEvents = "none";
 
 
-  checkRows()
-  checkColumns()
+  rowColumnsCheck()
   checkDiagonal()
 
-  whoWins = whoWins == undefined && matrix[0].includes("") == false && matrix[1].includes("") == false && matrix[2].includes("") == false ? "O jogo empatou" : whoWins;
+  whoWins = whoWins == undefined && gameBoard[0].includes("") == false && gameBoard[1].includes("") == false && gameBoard[2].includes("") == false ? "O JOGO EMPATOU" : whoWins;
 
   winnerText.innerText = whoWins;
 
@@ -55,88 +46,87 @@ function insertElement(element, position) {
   }
 }
 
-function checkColumns() {
+function rowColumnsCheck() {
 
-  for(let i = 0; i<3;i++) {
+  for(let row = 0; row<3;row++) {
     checkIfOWins = 0;
     checkIfXWins = 0;
-    for(let j = 0; j<3; j++) {
-      if(matrix[j][i] == "O") {
-        checkIfOWins++
-      }
 
-      else if(matrix[j][i] == "X") {
+    for(let column = 0; column<3; column++) {
+      if(gameBoard[row][column] == "X") {
         checkIfXWins++
       }
-
-      if(checkIfOWins==3 || checkIfXWins == 3) {
-        whoWins = checkIfOWins == 3 ? "O venceu" : "X venceu"
+      if(gameBoard[row][column] == "O") {
+        checkIfOWins++;
       }
+
+      if(checkIfXWins != 3 && checkIfOWins != 3 && column == 2) {
+        checkIfOWins = 0;
+        checkIfXWins = 0;
+        for(column=0; column<3;column++) {
+          if(gameBoard[column][row] == "X") {
+            checkIfXWins++;
+          }
+          if(gameBoard[column][row] == "O") {
+            checkIfOWins++;
+          }
+
+        }
+
+        column = 2
+      }
+
+      if(checkIfOWins == 3 || checkIfXWins == 3) {
+        whoWins = checkIfOWins == 3 ? "O VENCEU!" : "X VENCEU";
+        return;
+      }
+
     }
   }
 
-}
-
-function checkRows() {
-  for(let i = 0; i<3;i++) {
-    checkIfOWins = 0;
-    checkIfXWins = 0;
-    for(let j = 0; j<3; j++) {
-      if(matrix[i][j] == "O") {
-        checkIfOWins++
-      }
-
-      else if(matrix[i][j] == "X") {
-        checkIfXWins++
-      }
-
-      if(checkIfOWins==3 || checkIfXWins == 3) {
-        whoWins = checkIfOWins == 3 ? "O venceu" : "X venceu"
-      }
-    }
-  }
 }
 
 function checkDiagonal() {
-  if(matrix[0][0] == "X" && matrix[1][1] == "X" && matrix[2][2] == "X") {
-    whoWins = "X venceu"
-  }
+  checkIfXWins = 0
+  checkIfOWins = 0
+  for(let i = 0; i < 3; i++) {
+    if(gameBoard[i][i] != "") {
+      gameBoard[i][i] == "X" ? checkIfXWins++ : checkIfOWins++;
+    }
 
-  else if(matrix[0][2] == "X" && matrix[1][1] == "X" && matrix[2][0] == "X") {
-    whoWins = "X venceu"
-  }
+    if(i == 2 && checkIfXWins != 3 && checkIfOWins != 3) {
+      checkIfXWins = 0
+      checkIfOWins = 0
+      for (let j = 0; j < 3; j++) {
+        gameBoard[j][i] == "X" ? checkIfXWins++ : gameBoard[j][i] == "O" ? checkIfOWins++ : null;
+        i--;
+      }
+      i = 2;
+    }
 
-  else if(matrix[0][0] == "O" && matrix[1][1] == "O" && matrix[2][2] == "O") {
-    whoWins = "O venceu"
   }
-
-  else if(matrix[0][2] == "O" && matrix[1][1] == "O" && matrix[2][0] == "O") {
-    whoWins = "O venceu"
-  }
-
+  whoWins = checkIfXWins == 3 ? "X VENCEU!" : checkIfOWins == 3 ?  "O VENCEU!" : whoWins; 
 }
 
-function takeAnswer(answer) {
+function restartGame() {
   let images = document.querySelectorAll("img")
 
-  if(answer == "YES") {
-    for(let i = 0; i != images.length; i++) {
-      images[i].remove()
-    }
-
-    for(i = 0; i != squares.length; i++) {
-      squares[i].style.pointerEvents = "all"
-    }
-
-    popUp.style.opacity = 0;
-
-    setTimeout(() => {
-      popUp.style.display = "none";
-    }, 500);
-
-
-    matrix =  [["", "", ""],
-              ["", "", ""],
-              ["", "", ""]]    
+  for(let i = 0; i != images.length; i++) {
+    images[i].remove()
   }
+
+  for(i = 0; i != squares.length; i++) {
+    squares[i].style.pointerEvents = "all"
+  }
+
+  popUp.style.opacity = 0;
+
+  setTimeout(() => {
+    popUp.style.display = "none";
+  }, 300);
+
+
+  gameBoard =  [["", "", ""],
+            ["", "", ""],
+            ["", "", ""]]    
 }
